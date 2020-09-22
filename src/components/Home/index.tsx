31fe0caa3b6eb09c2be3089ceee284c8424f0a12
Loiklak <chauloic77@gmaiL.com>
2020-09-22 23:38:24 +0200

    Add an image inside the circle

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import * as Styled from './styles';
+import animeGirlPic from '../../assets/images/AnimeGirl.png';
 
 const Home = () => {
   return (
     <Styled.Container>
       <Styled.DotContainer>
-        <Styled.Dot />
+        <Styled.Dot>
+          <Styled.ImgInCircle src={animeGirlPic} />
+        </Styled.Dot>
       </Styled.DotContainer>
       <Styled.TextContainer>
+        <div />
         <Styled.BigText>Hello World</Styled.BigText>
       </Styled.TextContainer>
     </Styled.Container>
--- src/components/Home/styles.tsx
+++ src/components/Home/styles.tsx
@@ -7,7 +7,7 @@ export const Container = styled.div`
 export const TextContainer = styled.div`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   min-height: 100vh;
 `;
 
@@ -15,16 +15,6 @@ export const BigText = styled.p`
   font-size: 8em;
   color: white;
   z-index: 2;
-`;
-
-export const Dot = styled.span`
-  height: min(110vw, 110vh);
-  width: min(110vw, 110vh);
-  background-color: #239023;
-  border-radius: 50%;
-  display: inline-block;
-  position: relative;
-  top: -2vh;
 `;
 
 export const DotContainer = styled.div`
@@ -35,4 +25,20 @@ export const DotContainer = styled.div`
   height: 98vh;
   overflow: hidden;
   z-index: 1;
+`;
+
+export const Dot = styled.span`
+  height: min(110vw, 110vh);
+  width: min(110vw, 110vh);
+  background-color: #4f0099;
+  border-radius: 50%;
+  display: inline-block;
+  position: relative;
+  top: -2vh;
+  overflow: hidden;
+`;
+
+export const ImgInCircle = styled.img`
+  max-height: 100%;
+  max-width: 100%;
 `;
