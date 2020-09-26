2f1e84ed1d344d96858a68f141c685ead9240e0f
Loïc Chau <loicch@bam.tech>
2020-09-26 19:31:51 +0200

    Add real image

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import * as Styled from './styles';
-import animeGirlPic from '../../assets/images/AnimeGirl.png';
+import Pic from '../../assets/images/LoicNoBg.png';
 
 const Home = () => {
   return (
     <Styled.Container>
       <Styled.DotContainer>
         <Styled.Dot>
-          <Styled.ImgInCircle src={animeGirlPic} />
+          <Styled.ImgInCircle src={Pic} />
         </Styled.Dot>
       </Styled.DotContainer>
       <Styled.TextContainer>
-        <div />
         <Styled.BigText>Craftsman.</Styled.BigText>
       </Styled.TextContainer>
     </Styled.Container>
