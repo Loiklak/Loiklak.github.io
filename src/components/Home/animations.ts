3f5cb5d34c33d898be8554ece22e80f2a6decb23
Loiklak <chauloic77@gmaiL.com>
2020-09-27 22:31:39 +0200

    Create animation system

--- src/components/Home/animations.ts
+++ src/components/Home/animations.ts
@@ -28,3 +28,68 @@ export const FadeIn = keyframes`
     opacity: 1;
   }
 `;
+
+/**
+ * Animations timing
+ */
+
+/** Names of the elements we want to animate */
+export enum AnimatedElements {
+  BIG_DOT = 'bigDot',
+  BIG_PIC_ME = 'bigPicMe',
+  BIG_TEXT = 'bigText',
+}
+
+interface AnimationDescription {
+  name: AnimatedElements;
+  /** duration of the animation in seconds */
+  duration: number;
+}
+
+/** Animations and their duration ordered */
+const animationOrder: AnimationDescription[] = [
+  {
+    name: AnimatedElements.BIG_DOT,
+    duration: 1,
+  },
+  {
+    name: AnimatedElements.BIG_PIC_ME,
+    duration: 2,
+  },
+  {
+    name: AnimatedElements.BIG_TEXT,
+    duration: 2,
+  },
+];
+
+interface AnimationMappingElement {
+  animationDuration: string;
+  animationDelay: string;
+}
+interface AnimationMapping {
+  [animatedElement: string]: AnimationMappingElement;
+}
+
+/* Computes the delay for the animations in order to make them appear one by one */
+export const { animationMapping } = animationOrder.reduce<{
+  totalAnimationDuration: number;
+  animationMapping: AnimationMapping;
+}>(
+  (accumulator, currentAnimation) => ({
+    totalAnimationDuration:
+      accumulator.totalAnimationDuration + currentAnimation.duration,
+    animationMapping: {
+      ...accumulator.animationMapping,
+      [currentAnimation.name]: {
+        animationDuration: `${currentAnimation.duration}s`,
+        animationDelay: `${accumulator.totalAnimationDuration}s`,
+      },
+    },
+  }),
+  {
+    totalAnimationDuration: 0,
+    animationMapping: {},
+  }
+);
+
+console.log(animationMapping);
--- src/components/Home/styles.tsx
+++ src/components/Home/styles.tsx
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
-import * as animations from './animations';
+import {
+  DotGrow,
+  FadeIn,
+  AnimatedElements,
+  animationMapping,
+} from './animations';
 
 /**
  * Components
@@ -21,9 +26,11 @@ export const BigText = styled.span`
   font-size: ${(props) => props.theme.fontSize.headline};
   color: white;
   z-index: 2;
-  animation: ${animations.FadeIn} 2s;
+  animation: ${FadeIn}
+    ${animationMapping[AnimatedElements.BIG_TEXT].animationDuration};
   animation-fill-mode: forwards;
-  animation-delay: 2.7s;
+  animation-delay: ${animationMapping[AnimatedElements.BIG_TEXT]
+    .animationDelay};
   opacity: 0;
   position: relative;
   right: -20vw;
@@ -48,14 +55,17 @@ export const Dot = styled.span`
   align-items: flex-end;
   overflow: hidden;
   animation-fill-mode: forwards;
-  animation: ${animations.DotGrow} 1s ease-in-out;
+  animation: ${DotGrow}
+    ${animationMapping[AnimatedElements.BIG_DOT].animationDuration} ease-in-out;
 `;
 
 export const ImgInCircle = styled.img`
   max-height: 100%;
   max-width: 100%;
-  animation: ${animations.FadeIn} 2s linear;
-  animation-delay: 0.9s;
+  animation: ${FadeIn}
+    ${animationMapping[AnimatedElements.BIG_PIC_ME].animationDuration} linear;
+  animation-delay: ${animationMapping[AnimatedElements.BIG_PIC_ME]
+    .animationDelay};
   animation-fill-mode: forwards;
   opacity: 0;
   position: relative;
