13cf8fd8e19c7c745c279fbef22bb18c3898e7c6
Loiklak <chauloic77@gmaiL.com>
2020-10-31 17:24:50 +0100

    Create two columns responsive layout

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import * as Styled from './styles';
+
 export const About: React.FC = () => {
   const { t } = useTranslation();
-  return <div>{t('about.welcome')}</div>;
+  return (
+    <Styled.AboutContainer>
+      <Styled.TextContainer>{t('about.welcome')}</Styled.TextContainer>
+      <Styled.PictureContainer></Styled.PictureContainer>
+    </Styled.AboutContainer>
+  );
 };
---
+++ src/pages/About/styles.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const AboutContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  height: 100vh;
+`;
+
+export const TextContainer = styled.div`
+  flex: 2 1 400px;
+  background-color: red;
+`;
+
+export const PictureContainer = styled.div`
+  flex: 1 1 400px;
+  background-color: blue;
+`;
