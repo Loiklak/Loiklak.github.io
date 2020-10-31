0cb0df382e77f6450d54fee835aa2fe0524ceda6
Loiklak <chauloic77@gmaiL.com>
2020-10-31 21:06:21 +0100

    Fix theme

--- src/Theme.ts
+++ src/Theme.ts
@@ -10,11 +10,11 @@ export const theme: DefaultTheme = {
   // https://material.io/design/color/the-color-system.html#tools-for-picking-colors
   colors: {
     primary: '#1a73e8',
-    primaryDark: '##1f41b6',
-    primaryLight: '##34a4ff',
-    secondary: '##e88f1a',
-    secondaryDark: '##d15b0e',
-    secondaryLight: '##f0b755',
+    primaryDark: '#1f41b6',
+    primaryLight: '#34a4ff',
+    secondary: '#e88f1a',
+    secondaryDark: '#d15b0e',
+    secondaryLight: '#f0b755',
     background: '#ffffff',
     text: 'black',
     shadow: '#cecece',
