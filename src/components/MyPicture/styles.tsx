de8dea004de725fdb3b89279f6e369611c27ede0
Loiklak <chauloic77@gmaiL.com>
2020-10-31 18:00:00 +0100

    Create Picture component

---
+++ src/components/MyPicture/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import img from '../../assets/images/LoicNoBg.png';
+
+export const PictureOfMine = styled.div`
+  height: 100%;
+  width: 100%;
+  background-image: url(${img});
+  background-size: 100%;
+`;
