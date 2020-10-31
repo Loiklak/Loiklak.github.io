60b3d3160fdc16ae6faf8b1311f0d76c58f0cf83
Loiklak <chauloic77@gmaiL.com>
2020-10-31 21:36:40 +0100

    Create text component

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { DefaultTheme } from 'styled-components';
+
+import * as Styled from './styles';
+
+interface PropsType {
+  className?: string;
+  color?: keyof DefaultTheme['colors'];
+  size?: keyof DefaultTheme['fontSize'];
+  children: string;
+}
+
+export const Text: React.FC<PropsType> = ({
+  className,
+  color = 'text',
+  size = 'small',
+  children,
+}: PropsType) => {
+  return (
+    <Styled.Text color={color} size={size} className={className}>
+      {children}
+    </Styled.Text>
+  );
+};
