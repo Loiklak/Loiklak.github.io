90cf05b24864fc14e0457514d8d58b9985c9ecee
Loiklak <chauloic77@gmaiL.com>
2020-11-01 15:24:06 +0100

    Create computer and mobile pic component

---
+++ src/components/ComputerAndMobilePicture/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import * as Styled from './styles';
+
+interface PropsType {
+  className?: string;
+}
+
+export const MyPicture: React.FC<PropsType> = (props: PropsType) => {
+  return <Styled.Pic className={props?.className} />;
+};
---
+++ src/components/ComputerAndMobilePicture/styles.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import img from '../../assets/images/ComputerAndMobile.jpg';
+
+export const Pic = styled.div`
+  height: 100%;
+  width: 100%;
+  background-image: url(${img});
+  background-size: 100%;
+`;
