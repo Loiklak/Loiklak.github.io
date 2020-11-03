a01b0808e23ceff6fa835b4c4a8c37e8f9422806
Loiklak <chauloic77@gmaiL.com>
2020-11-03 22:14:34 +0100

    Updates

---
+++ precache-manifest.6c813dd3b00de50dad54598b797e5b32.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4131e8a7c62e77e6bbdcd0c3fbc909d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a491fc3639791951636d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7d4e0c327f1636854baf",
+    "url": "/static/js/2.002b658d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.002b658d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a491fc3639791951636d",
+    "url": "/static/js/main.9bbf0ead.chunk.js"
+  },
+  {
+    "revision": "f172093a2682716b29e2",
+    "url": "/static/js/runtime-main.5cf18ba1.js"
+  },
+  {
+    "revision": "f94706e8ceb56c8f26235a99e640c667",
+    "url": "/static/media/ComputerAndMobile.f94706e8.jpg"
+  },
+  {
+    "revision": "0a366da1dc1f1a19520c6cd1774985d2",
+    "url": "/static/media/LoicNoBg.0a366da1.png"
+  }
+]);
