12a48705042013212ee02ddfb16e6419c71ec11a
Loiklak <chauloic77@gmaiL.com>
2020-11-04 22:55:59 +0100

    Nest i18n keys

--- src/Translation/en.ts
+++ src/Translation/en.ts
@@ -1,20 +1,23 @@
 export const enTranslations = {
   translation: {
-    'intro.welcome': 'Hey 👋😉',
-    'intro.iAm': "I'm ",
-    'intro.LoicChau': 'Loïc Chau',
-    'intro.whatIAm': 'Software engineer',
-    'intro.downloadCv': 'Download CV',
-    'about.myDriveBeginning':
-      'What drives me is helping people achieve their goals',
-    'about.and': 'and',
-    'about.faster': 'faster',
-    'about.easier': 'easier',
-    'about.gamingLedTo':
-      'Spending time on my computer gaming thus led me to become interested in',
-    'about.softwareEngineering': 'software engineering',
-    'navbar.resume': 'Resume',
-    'navbar.about': 'About me',
-    'navbar.projects': 'My Projects',
+    intro: {
+      welcome: 'Hey 👋😉',
+      iAm: "I'm ",
+      LoicChau: 'Loïc Chau',
+      whatIAm: 'Software engineer',
+      downloadCv: 'Download CV',
+    },
+    about: {
+      part1:
+        "Enthusiastic gamer and tryharder, I consider 'Practice makes it perfect' as one of my moto.",
+      part2: 'Very open minded, I love learning and helping others.',
+      part3:
+        'The thing I deem most important in whatever I do is having a tangible and positive impact on the target of my actions.',
+    },
+    navbar: {
+      resume: 'Resume',
+      about: 'About me',
+      projects: 'My Projects',
+    },
   },
 };
--- src/Translation/i18n.ts
+++ src/Translation/i18n.ts
@@ -13,9 +13,6 @@ i18n
   .init({
     resources,
     lng: 'en',
-
-    keySeparator: false, // we do not use keys in form messages.welcome
-
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
